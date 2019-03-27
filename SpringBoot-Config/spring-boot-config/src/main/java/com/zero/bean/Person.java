555430ea1687a61e4751342d47bf05c26525b08e
96pzero <46878251+96pzero@users.noreply.github.com>
2019-03-27 14:34:30 +0800

    Create PropertySource
    @PropertySource 使用指定配置文件的内容，前提是application中没有设置。

--- SpringBoot-Config/spring-boot-config/src/main/java/com/zero/bean/Person.java
+++ SpringBoot-Config/spring-boot-config/src/main/java/com/zero/bean/Person.java
@@ -1,6 +1,7 @@
 package com.zero.bean;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,6 +13,7 @@ import java.util.Map;
  * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定；
  *      prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
  */
+@PropertySource(value = {"classpath:/person.properties"})// 指定调用哪一份配置文件
 @Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
