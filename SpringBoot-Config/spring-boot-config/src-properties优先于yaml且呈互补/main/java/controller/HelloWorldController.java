0eb7bc8df38e78cb350f02e581f50b04dbecd202
96pzero <46878251+96pzero@users.noreply.github.com>
2019-03-28 15:18:46 +0800

    Create Config001
    properties优先于yaml且呈互补。

---
+++ SpringBoot-Config/spring-boot-config/src-properties优先于yaml且呈互补/main/java/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.zero.controller;
+
+import com.zero.bean.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    Person person;
+    @RequestMapping("/hello")
+    public Person hello(){
+        return person;
+    }
+}
